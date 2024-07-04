ff1bfd21016d5fd2331c104fedfc1a9bc07b7fd1
Francisco Neto <f.neto@outlook.com>
2024-07-04 01:09:50 -0300

    Criando componentes funcionais e aplicando Hook de estado

---
+++ 13-react-II/todo-list/src/components/Tasks/Tasks.tsx
@@ -0,0 +1,68 @@
+import { FormEvent, useState } from "react";
+import styles from "./styles.module.scss";
+
+export const Tasks: React.FC = () => {
+  interface Task {
+    id: number;
+    title: string;
+    done: boolean;
+  }
+
+  const [taskTitle, setTasktTitle] = useState("");
+  const [tasks, setTasks] = useState([] as Task[]);
+
+  /**
+   * Função disparada quando se submete o formulário
+   * @param event
+   */
+  function handleSubmitAddTask(event: FormEvent) {
+    event.preventDefault();
+
+    if (taskTitle.length < 3) {
+      alert("Não é possível adicionar uma tarefa com menos de 3 letras");
+      return;
+    }
+
+    setTasks([
+      ...tasks, // pega todos os valores que já existiam em tasks e adiciona esse novo array que está sendo setado no estado tasks
+      {
+        id: new Date().getTime(),
+        title: taskTitle,
+        done: false,
+      },
+    ]);
+
+    setTasktTitle("");
+  }
+
+  return (
+    <section className={styles.container}>
+      <form onSubmit={handleSubmitAddTask}>
+        <div>
+          <label htmlFor="task-title">Adicionar Tarefa</label>
+          <input
+            value={taskTitle}
+            type="text"
+            name=""
+            id="task-title"
+            onChange={(event) => {
+              setTasktTitle(event.target.value);
+            }}
+            placeholder="Título da Tarefa"
+          />
+        </div>
+        <button type="submit">Adicionar</button>
+      </form>
+      <ul>
+        {tasks.map((task) => {
+          return (
+            <li key={task.id}>
+              <input type="checkbox" name="" id={String(task.id)} />
+              <label htmlFor={String(task.id)}>{task.title}</label>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
